95fdf1de6313a3b3f9daaa6dc31b3d89c18842c6
creyes <109313036+creyes027@users.noreply.github.com>
2023-03-15 16:41:53 +0000

    Fixed main function

--- main.py
+++ main.py
@@ -10,13 +10,11 @@ def main():
 
     if(number1 < number2 and number1 < number3):
         lowest = number1
-        print(f'The lowest number is: {lowest} ')
     elif(number2 < number1 and number1 < number3):
         lowest = number2
-        print(f'The lowest number is: {lowest} ')
     else:
         lowest = number3
-        print(f'The lowest numbr is : {lowest} ')
-
+        
+    print(f'The lowest number is {lowest} ')
 if __name__ == '__main__':
     main()
